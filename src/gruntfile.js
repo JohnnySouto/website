00e67ce018cdec3fe95fdb02edc80c0612903b11
Johnny Souto <johnny.souto@tedbrasil.com>
2017-02-15 15:55:54 -0200

    mais organização de arquivos e configuração do gruntfile.js

---
+++ src/gruntfile.js
@@ -0,0 +1,13 @@
+global.origemPath = 'src/app/';
+global.destinoPath = 'public_html/'
+
+module.exports = function(grunt){
+
+    // Carregando tarefas em diretorios
+    grunt.loadTasks('src/tasks/index');
+
+    //Configuração inicial do grunt
+    grunt.initConfig({
+
+    });
+};
