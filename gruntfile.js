a9f9afe6b521cf2f323d080985c7e0278efb4796
Johnny Souto <johnnyjns@gmail.com>
2017-03-17 17:09:53 -0300

    ajustes e mais ajustes

--- gruntfile.js
+++ gruntfile.js
@@ -1,17 +1,20 @@
 global.origemPath = 'src/app/';
-global.destinoPath = 'public_html/'
+global.destinoPath = 'public_html/';
+global.testePath = 'teste/';
+global.outroPath = 'outro-src/';
 
 module.exports = function(grunt){
-
-    // Carregando tarefas em diretorios
-    grunt.loadTasks('src/tasks/index');
 
     //Configuração inicial do grunt
     grunt.initConfig({
         clean: {
 
-            destino: {
+            public: {
                 src: global.destinoPath
+            },
+
+            teste: {
+              src: global.testePath
             }
         },
 
@@ -31,11 +34,18 @@ module.exports = function(grunt){
                 dest: global.destinoPath
             },
 
+            teste: {
+              expand: true,
+              cwd: global.outroPath,
+              src:'**',
+              dest: global.testePath
+            }
+
         }
 
     });
 
-    // console.log(global.origemPath + 'index.html');
+    console.log(global.origemPath + 'index.html');
     // console.log(global.destinoPath);
 
     //Carregando tarefas do NPM
@@ -43,6 +53,7 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-clean');
 
     //Registra tarefas
-    grunt.registerTask('dist', ['clean', 'copy']);
+    grunt.registerTask('dist', ['clean:public', 'copy:public']);
+    grunt.registerTask('teste', ['clean:teste', 'copy:teste']);
     grunt.registerTask('default', ['copy:index']);
 };
