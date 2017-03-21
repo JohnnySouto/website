397d3893c7a36176a86bc6956a203b92b7cdb4f6
Johnny Souto <johnnyjns@gmail.com>
2017-03-21 00:23:16 -0300

    mais ajustes

--- gruntfile.js
+++ gruntfile.js
@@ -5,6 +5,11 @@ global.outroPath = 'outro-src/';
 
 module.exports = function(grunt){
 
+  //Carregando tarefas do NPM
+  grunt.loadNpmTasks('grunt-chokidar');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
     //Configuração inicial do grunt
     grunt.initConfig({
         clean: {
@@ -27,13 +32,6 @@ module.exports = function(grunt){
                 dest: global.destinoPath
             },
 
-            index: {
-                expand: true,
-                cwd: global.origemPath,
-                src: 'index.html',
-                dest: global.destinoPath
-            },
-
             teste: {
               expand: true,
               cwd: global.outroPath,
@@ -45,15 +43,38 @@ module.exports = function(grunt){
 
     });
 
-    console.log(global.origemPath + 'index.html');
+    //Tarefa que cria o arquivo index.html inicial
+    grunt.registerTask('index', 'Cria o index', function(){
+      var index = '';
+
+      index += '<!DOCTYPE html>';
+      index += '<html lang="pt-BR">';
+      index += '<head>';
+      index += '<title>Johnny Souto - Verificando idioma ...</title>';
+      index += '<meta charset="utf-8">';
+      index += '<meta name="robots" content="noindex">';
+      index += '</head>';
+      index += '<body>';
+      index += '<script>';
+      index += 'var lang = navigator.language || navigator.userLanguage;';
+      index += 'if(lang == 'en-US'){';
+      index += 'window.location.replace("./en");';
+      index += '} else {';
+      index += 'window.location.replace("./br");';
+      index += '}';
+      index += 'console.log("Navegador esta no idioma: " + lang);';
+      index += '</script></body></html>';
+
+      grunt.file.write(origemPath + 'outro.html', index);
+    });
+
+    // console.log(global.origemPath + 'index.html');
     // console.log(global.destinoPath);
 
-    //Carregando tarefas do NPM
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
 
     //Registra tarefas
     grunt.registerTask('dist', ['clean:public', 'copy:public']);
-    grunt.registerTask('teste', ['clean:teste', 'copy:teste']);
-    grunt.registerTask('default', ['copy:index']);
+    grunt.registerTask('dist-teste', ['clean:teste', 'copy:teste']);
+    grunt.registerTask('index');
+    grunt.registerTask('default', ['chokidar']);
 };
